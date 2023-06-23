31597329f51c2022432e6be49e3dd9b68b1a3a6e
PaulKagan <38296737+PaulKagan@users.noreply.github.com>
2023-06-23 18:34:40 +0000

    Still setting up

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const db = require('mongoose');
 const app = express();
 
 
+// Setting up server config.
+
 
 app.use(express.static("public"));
-app.use(bp.urlencoded({extended:false}))
+app.use(bp.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
+
+// Setting up DB.
+
 // const url = "mongodb+srv://mishrb20:mishrb20@cluster0.9t512bo.mongodb.net/SVshop";
-const url = "mongodb+srv://mishrb20:mishrb20@cluster0.9t512bo.mongodb.net/SVshop";
+const url = "mongodb+srv://pkagan:mdbpw123@cluster0.wsme1wo.mongodb.net/SVCShop";
 
 db.connect(url)
   .then(() => {
@@ -21,37 +26,94 @@ db.connect(url)
   });
 
 
+// Setting up schemas and models.
 
-  
-  const userSchema = db.Schema({
-      userName: String,
-      email: String,
-      password: String
-      
-    });
-    
-    const userModel = db.model('user',userSchema);
+const userSchema = db.Schema({
+  userName: String,
+  email: String,
+  password: String
 
- // im not really sure that we suppose to do the next schema's
-//  yeah, we need them. paul.
+});
 
-  const productSchema = db.Schema({
-    productName: String,
-    productPrice: Number
-    });
+const userModel = db.model('user', userSchema);
 
-    const productModel = db.model('product',productSchema);
+//? im not really sure that we suppose to do the next schema's
+//?  yeah, we need them. paul.
 
- 
-    const orderSchema = db.schema({
-        totalProducts: Number,
-        totalPrice: Number,
-        costumerName: String // we need to figure out if this suposed to be an obj.
-    })
+const productSchema = db.Schema({
+  productName: String,
+  productPrice: Number
+});
 
-    const orderModel = db.models('order', orderSchema);
+const productModel = db.model('product', productSchema);
 
 
+const orderSchema = db.Schema({
+  totalProducts: Number,
+  totalPrice: Number,
+  costumerName: String //? we need to figure out if this suposed to be an obj.
+});
+
+const orderModel = db.model('order', orderSchema);
+
+// Server 
+
+// Sign in page
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+  console.log('hello');
+});
+
+app.post('/', async (req, res) => {
+  console.log('dummy');
+})
+
+
+// Sign up page
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/public/signup.html');
+});
+
+app.post('/signup', async (req, res) => {
+  console.log('dummy');
+});
+
+
+//? Michell, why do we have products page and all page? what is the difference?
+// Producs page
+
+app.get('/products', (req, res) => {
+  res.sendFile(__dirname + '/public/products.html');
+});
+
+app.post('/products', async (req, res) =>{
+  console.log('dummy');
+});
+
+
+// All page
+
+app.get('/all', (req, res) => {
+  res.sendFile(__dirname + '/public/all.html');
+});
+
+app.post('/all', async (req, res) =>{
+  console.log('dummy');
+});
+
+
+// checkout page
+//? I assume buy was supposed to be the checkout page?
+
+app.get('/checkout', (req, res) => {
+  res.sendFile(__dirname + '/public/checkout.html');
+});
+
+app.post('/checkout', async (req, res) =>{
+  console.log('dummy');
+});
 
 
 
@@ -73,27 +135,9 @@ db.connect(url)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('running on port 3000')})
+app.listen(3000, () => {
+    console.log('running on port 3000')
+})
 
     // עמוד ראשי (ערוץ '/'):
 
