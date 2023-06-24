a164f50b42d3b3895d88a3891ab71bc1f072c7a2
PaulKagan <38296737+PaulKagan@users.noreply.github.com>
2023-06-24 17:08:19 +0000

    Co-authored-by: mishmish11 <mishmish11@users.noreply.github.com>

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(bp.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
+app.use(bp.json())
 
 
 // Setting up DB.
@@ -78,22 +78,26 @@ app.get('/signup', (req, res) => {
 
 app.post('/signup', async (req, res) => {
   const user = {
-    userName: req.body.usersName,
+    userName: req.body.userName,
     email: req.body.email,
     password: req.body.password
   };
-  await userModel.findOne({userName: user.userName})
-  .then((existing)=>{   
-    if (existing != undefined || existing != null) {
-      alert('User exists');
-      return;
-    }
-  });
+  console.log("1. user: "+user.userName + " " + user.email + " " + user.password);
+  const existing = await userModel.findOne({email: user.email});
+  console.log("existing: "+existing);
+  if (existing != null) {
+    console.log('User exists');
+    res.json({message: 'User already exists!'});
+  } else {
+    console.log('no such user');
+    await userModel.create(user).then(() => {
+      console.log("redirect");
+      // res.setHeader("Content-Type", "text/html");
+      // res.redirect(200,'/home');
+      //! redirect suddenly isnt working.
+    }).catch((err) => console.log(err)); 
+  }
 
-  await userModel.create(user).then(() => {
-    res.redirect(__dirname + '/public/home.html');
-  }).catch(console.log(err));
-  console.log('dummy');
 });
 
 
@@ -133,12 +137,75 @@ app.post('/checkout', async (req, res) =>{
 
 
 
+//TERMS OF USE PAGE
+
+app.get('/tos',(req,res) => {
+  res.sendFile(__dirname + '/public/tos.html')
+})
 
 
 
 
 
 
+
+
+// app.get('/addProducts', async (req,res) => {
+
+
+//   const products = [{productName:"Zara shirt", productPrice: 110 },
+//    {productName:"Gucci shirt", productPrice:650 },
+//   {productName:"Louis vitton shirt",productName:750},
+// {productName:"Castro shirt", productPrice: 50},
+
+// {productName:"CK belt", productPrice: 250},
+// {productName:"LV belt", productPrice: 2300},
+// {productName:"Hermess belt", productPrice:3500},
+
+// {productName:"Adidas shoes", productPrice:350},
+// {productName:"Nike shoes", productPrice:450},
+// {productName:"New balanc shoes", productPrice:280},
+// {productName:"Basic jordan shoes", productPrice:500},
+// {productName:"Vintage jordan shoes", productPrice:1500},
+
+// {productName:"New Era cap", productPrice: 150},
+// {productName:"Nike cap", productPrice:120},
+// {productName:"Adidas cap", productPrice:120},
+
+// {productName:"MJ jeans", productPrice:300},
+// {productName:"Craieser jeans", productPrice: 450},
+// {productName:"Disel jeans", productPrice:850},
+// {productName:"Lee cooper jeans",productPrice:380},
+// {productName:"Zara jeans",productPrice:200},
+// {productName:"Renuar jeans",productPrice:150},
+// {productName:"Castro jeans",productPrice:120},
+
+// {productName:"Zara jacket",productPrice:250},
+// {productName:"Renuar jacket",productPrice:250},
+// {productName:"Bershka ONE PIECE jacket",productPrice:200},
+// {productName:"Denim jacket", productPrice:350},
+
+// {productName:"Zara swimsuit",productPrice: 100},
+// {productName:"Castro swimsuit",productPrice:80},
+// {productName:"Bershka swimsuit",productPrice:85},
+// {productName:"Basic swimsuit",productPrice:50},
+
+// {productName:"Silver ring",productPrice:130},
+// {productName:"Gold ring",productPrice:650},
+// {productName:"gold neckless",productPrice:3200},
+
+// {productName:"Castro boxers",productPrice:60},
+// {productName:"CK boxers",productPrice:200},
+// {productName:"Pierr cardin boxers",productPrice:100},
+
+// {productName:"Cotton socks",productPrice:40},
+// {productName:"No show socks",productPrice: 40},
+// {productName:"Long socks ",productPrice:40}
+
+
+// ]
+// await productModel.insertMany(products)
+// })
 
 
 
@@ -153,7 +220,7 @@ app.post('/checkout', async (req, res) =>{
 
 app.listen(3000, () => {
     console.log('running on port 3000')
-})
+});
 
     // עמוד ראשי (ערוץ '/'):
 
