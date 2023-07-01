dc881bea29f046ace93fc37a963686aa9f412fdf
PaulKagan <38296737+PaulKagan@users.noreply.github.com>
2023-07-01 10:53:59 +0000

    style

--- public/product-script.js
+++ public/product-script.js
@@ -1,51 +1,61 @@
 let products = [];
-const showProducts = () => {
+const getProducts = () => {
     fetch('/showProducts').then((res) => res.json())
         .then(data => {
             products = data.productList;
-            elementsForEach(products);
+            showProducts(products);
         });
         
         
 }
     
-showProducts()
+getProducts();
     
     
-sortItemsByPrice()
+const sortItemsByPrice = () => {
+
+};
     
     
     
-sortItemsByName()
+const sortItemsByName = () => {
+
+};
 
 
-    const elementsForEach = productsArray =>{
-        const productList = document.getElementById('product-list');
-        productsArray.forEach((product =>{
-            // Create a div element for the product
-            const productDiv = document.createElement('div');
-            productDiv.style.backgroundImage = `url(${product.image})`;
-            productDiv.classList.add('productDiv');
+const showProducts = productsArray =>{
+    const productList = document.getElementById('product-list');
+    productsArray.forEach((product =>{
+
+        const productContainerDiv = document.createElement('div');
+        productContainerDiv.style.backgroundColor = `lightgray`;
+        productContainerDiv.classList.add('productContainerDiv');
+
+        // Create a div element for the product
+        const productDiv = document.createElement('div');
+        productDiv.style.backgroundImage = `url(${product.image})`;
+        productDiv.classList.add('productDiv');
+    
+        // Create a span element for the product name
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = product.productName + ` - $${product.productPrice}`;
+        nameSpan.classList.add("prod-top");
+        productDiv.appendChild(nameSpan);
         
-            // Create a span element for the product name
-            const nameSpan = document.createElement('span');
-            nameSpan.textContent = product.productName + ` - $${product.productPrice}`;
-            nameSpan.classList.add("prod-top");
-            productDiv.appendChild(nameSpan);
-            
-            // Create a button element for "Add to Cart"
-            const addToCartButton = document.createElement('button');
-            addToCartButton.textContent = 'Add to Cart';
-            
-            // Add an event listener to the button
-            addToCartButton.addEventListener('onclick', function () {
-                // Add your logic for adding the product to the cart here
-                console.log('Product added to cart:', product.name);
-            });
-            
-            productDiv.appendChild(addToCartButton);
-            addToCartButton.classList.add("prod-buttom");
-            // Append the product div to the product list container
-            productList.appendChild(productDiv);
-        }));
-    } 
+        // Create a button element for "Add to Cart"
+        const addToCartButton = document.createElement('button');
+        addToCartButton.classList.add("prod-buttom");
+        addToCartButton.textContent = 'Add to Cart';
+        
+        // Add an event listener to the button
+        addToCartButton.addEventListener('onclick', function () {
+            // Add your logic for adding the product to the cart here
+            console.log('Product added to cart:', product.name);
+        });
+        
+        productDiv.appendChild(addToCartButton);
+        // Append the product div to the product list container
+        productContainerDiv.appendChild(productDiv);
+        productList.appendChild(productContainerDiv);
+    }));
+} ;
